976267d66450cba3b5c1f020d11d9a14e95e5eaf
benikabocha <benikabocha@gmail.com>
2017-01-21 00:06:29 +0900

    確認スクリプトにコメント追加

--- sample02.py
+++ sample02.py
@@ -2,11 +2,14 @@
 
 import sample02
 
-print('sample02.add', sample02.add(1, 2))
+# 関数の呼び出しをテスト
+print('sample02.add(1, 2):', sample02.add(1, 2))
 
+# sample02 で作成したクラスをテスト
 dog = sample02.Dog()
 print('sample02.call_go(dog):', sample02.call_go(dog))
 
+# sample02.Animal を継承するテスト
 class Cat(sample02.Animal):
     def go(self, n_times):
         return "ニャー! " * n_times
