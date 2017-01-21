77327012f34950ea47b091be8f1c0c125e8ffd28
benikabocha <benikabocha@gmail.com>
2017-01-21 20:18:48 +0900

    NULL を nullptr に変更

--- sample01.cpp
+++ sample01.cpp
@@ -6,8 +6,8 @@
 namespace py = pybind11;
 
 int main(int argc, char** argv) {
-	wchar_t* program = Py_DecodeLocale(argv[0], NULL);
-	if (program == NULL) {
+	wchar_t* program = Py_DecodeLocale(argv[0], nullptr);
+	if (program == nullptr) {
 		fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
 		exit(1);
 	}
--- sample03.cpp
+++ sample03.cpp
@@ -43,8 +43,8 @@ PyMODINIT_FUNC PyInit_sample03() {
 }
 
 int main(int argc, char** argv) {
-	wchar_t* program = Py_DecodeLocale(argv[0], NULL);
-	if (program == NULL) {
+	wchar_t* program = Py_DecodeLocale(argv[0], nullptr);
+	if (program == nullptr) {
 		fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
 		exit(1);
 	}
